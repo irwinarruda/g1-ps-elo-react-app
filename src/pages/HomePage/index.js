03d67cb6c45b8a71c023d6264eb5b67aff43ffd7
Caio Oliveira <caio.goncalves.pro@gmail.com>
2020-11-03 18:41:24 -0300

    criacao folder HomePage e estilizacao MinhaConta

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header/index';
+import './styles.css';
+
+function HomePage(){
+    return(
+        <div className="fundo">
+          <Header />
+        </div>        
+    )
+}
+
+export default HomePage;
