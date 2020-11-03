4998d6ca8362490d18c28e009bd6a75e4e1c95b3
Vitória Justino <vitoria.m.justino@gmail.com>
2020-11-03 16:58:32 -0300

    Login e Cadastro finalizados

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './styles.css';
 
-function Input({label, type, name, value, setValue, error}) {
+function Input({label, type, name, value, setValue, error, ...props}) {
     function handleChange (event) {
         setValue(event.target.value)
     }
     return (
         <div>
            <label className="input-label">{label}</label> 
-           <input className="input-input" type={type} name={name} id={name} value={value} onChange={handleChange}/>
+           <input {...props} className="input-input" type={type} name={name} id={name} value={value} onChange={handleChange}/>
         {error? <p className="input-error">{error}</p>: null}
         </div>
     );
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,27 +1,59 @@
 import React from 'react';
 import './styles.css';
+import {Redirect} from 'react-router-dom';
 import Input from '../../components/Input';
 import LoginImg from '../../assets/login.svg';
 import LogoNomeImg from '../../assets/logo-nome.svg';
+import WaitImg from '../../assets/wait.svg'
 import {Link} from 'react-router-dom';
+import {USER_LOGIN} from '../../UserApi';
 
 function Login() {
     const [email, setEmail] = React.useState ("");
     const [password, setPassword] = React.useState ("");
+    const [wait, setWait] = React.useState(false);
+    const [redirect, setRedirect] = React.useState(false);
+    function renderRedirect(){
+        if(redirect) {
+            return <Redirect to='/'/>
+        }
+    }
+    async function handleSubmit(event){
+        try{
+            event.preventDefault();
+            setWait(true);
+            const body = {
+                email: email,
+                password: password
+            };
+            const {url, options} = USER_LOGIN(body);
+            console.log(url); console.log(options);
+            const response = await fetch (url, options);
+            const json = await response.json();
+            console.log(json);
+            setRedirect(true);
+        } catch(err) {
+            console.error("Erro: " +err);
+        } finally {
+            setWait(false);
+        }
+    }
     return (
         <div className="cordefundo">
             <div className="login-container">
                 <div className="logo-ilustracao">
-                    <img src={LoginImg} />
+                    <img src={LoginImg} alt="Login" />
                 </div>
                 <div className="login-cadastro">
-                    <img src={LogoNomeImg}/>
-                    <form>
-                        <Input type="text" name="email" label="E-mail:" value={email} setValue={setEmail}/>
+                    <img src={LogoNomeImg} alt="Count and Flix"/>
+                    <form onSubmit={handleSubmit}>
+                        <Input type="email" name="email" label="E-mail:" value={email} setValue={setEmail}/>
                         <Input type="password" name="password" label="Senha:" value={password} setValue={setPassword}/>
-                        <button className="btn">Entrar</button>
+                        {!wait? <button type="submit" className="btn">Entrar</button>: <button type="submit" className="btn" disabled="disabled">Entrar</button>}
                     </form>
-                    <Link to="/criar">Criar uma conta</Link>
+                    <Link to="/cadastro">Criar uma conta</Link>
+                    {wait?<img className="waitCircle" src={WaitImg} alt="Carregamento" />:null}
+                    {renderRedirect()}
                 </div>
             </div>
         </div>
